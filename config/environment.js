d98103f5cdf9b4d2a236304f00941d707c8ed589
Kellen Kolbeck & Kyle Mellander <student@epicodus.com>
2015-09-28 09:52:26 -0700

    Add model for categories and display on index page

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'kellenslist',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://kellenslist.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
