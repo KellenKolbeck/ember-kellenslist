525ebbdc173b22da20a2ced9549b9257cff3ea34
Kellen Kolbeck & Kyle Mellander <student@epicodus.com>
2015-09-29 10:07:32 -0700

    Add verification for adding post

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -9,14 +9,29 @@ export default Ember.Component.extend({
       $("input.category-input").val(categoryName);
     },
     addPost() {
-      var params = {
-        category: this.categoryRecord,
-        post_name: this.get('post-name'),
-        post_body: this.get('post-body'),
-        post_images: this.get('post-image'),
-        created_at: new Date()
+      var error = "";
+      if(this.categoryRecord=== "") {
+        error += '<div class="alert alert-danger">You must select a category</div>'
       };
-      this.sendAction("addPost", params);
+      if(this.get('post-name') === "" || this.get('post-name') === undefined) {
+        error += '<div class="alert alert-danger">You must enter a title for your post</div>'
+      }
+      if(this.get('post-body') === "" || this.get('post-body') === undefined) {
+        error += '<div class="alert alert-danger">You must enter a body for your post</div>'
+      }
+      if(error!=="") {
+        $(".main-container").prepend(error);
+        $(".alert.alert-danger").delay(3000).fadeOut(1000, function() {$(this).remove();});
+      } else {
+        var params = {
+          category: this.categoryRecord,
+          post_name: this.get('post-name'),
+          post_body: this.get('post-body'),
+          post_images: this.get('post-image'),
+          created_at: new Date()
+        };
+        this.sendAction("addPost", params);
+      }
     }
   }
 });
